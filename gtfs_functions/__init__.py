012f4c7c6c38f46a1af293b3a56e3529d194310c
santiago.toso <santiago.toso@ridewithvia.com>
2023-03-13 15:04:43 +0100

    package-ready changes

---
+++ gtfs_functions/__init__.py
@@ -0,0 +1,2 @@
+from gtfs_functions.gtfs_functions import Feed
+from gtfs_functions.gtfs_plots import map_gdf
--- src/gtfs_functions/__init__.py
+++
@@ -1,7 +0,0 @@
-from gtfs_functions.gtfs_functions import (
-    Feed)
-from gtfs_functions.aux_functions import (
-    add_runtime, add_distance, add_speed, fix_outliers,
-    aggregate_speed, add_all_lines_speed, add_free_flow, add_all_lines,
-    label_creation, window_creation, add_frequency, add_route_name)
-from gtfs_functions.gtfs_plots import map_gdf
