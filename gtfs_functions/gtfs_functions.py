b26a80d28f1cec9e7653ee62724951e2c4a501cf
Patrick Pichler <patrick.pichler@creativedata.ioGit config --global user.email patrick.pichler@creativedata.io>
2024-01-08 09:55:36 +0100

    Fix OSError if gtfs_path is URL for extract_file

--- gtfs_functions/gtfs_functions.py
+++ gtfs_functions/gtfs_functions.py
@@ -1146,7 +1146,7 @@ def extract_file(file, feed):
                 return logging.info(f'File "{file}.txt" not found.')     
         
         # Try as a URL
-        except FileNotFoundError as e:
+        except (FileNotFoundError, OSError) as e:
             if f'{file}.txt' in files:
                 r = requests.get(gtfs_path)
                 with ZipFile(io.BytesIO(r.content)) as myzip:
